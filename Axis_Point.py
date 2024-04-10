09b4b1fd68668ec4773a211403a846eb8e1738fa
OanceaGabriel <gabriel.oancea400@gmail.com>
2024-04-10 17:17:43 +0300

    Added Exercise File, Read functionality, and tester center of mass and area calculation, all is OK

--- Axis_Point.py
+++ Axis_Point.py
@@ -5,8 +5,10 @@ class Axis_Point:
     # x represents the distance along the beam where the point is placed
     x = None
     # the container for every load present in this point (ex: Force, Bending Moment etc.)
-    loads = []
+    forces = []
+    bending_moments = []
 
-    def __init__(self, x, loads):
+    def __init__(self, x, forces=[], bending_moments=[]):
         self.x = x
-        self.loads = loads
+        self.forces = forces
+        self.bending_moments = bending_moments
