64705dd492cd47afc70822b6366d61bf9f08c513
OanceaGabriel <gabriel.oancea400@gmail.com>
2024-04-15 12:04:59 +0300

    Added Axis points to exercise_01, Read from file and created Axis Point objects, Created Segment objects and segment list, Fixed integrity check, Changed Support Class and reduced 1 from dof of each Support to account for the fact we don't use axial internal forces.

--- Segment.py
+++ Segment.py
@@ -4,13 +4,14 @@ class Segment:
     point_1 = None
     point_2 = None
     length = None
-    distributed_force = None
+    distributed_force = 0
 
-    def __init__(self, point_1, point_2, distributed_force=False):
+    def __init__(self, point_1, point_2, distributed_force=0):
         self.point_1 = point_1
         self.point_2 = point_2
         self.length = self.point_2.x - self.point_1.x
-        self.distributed_force = distributed_force
+        if point_1.distributed_force != 0 and point_2.distributed_force != 0:
+            self.distributed_force = distributed_force
 
     #The function used to plot the shear forces graph
     def s_function(self):
--- Support.py
+++ Support.py
@@ -1,19 +1,19 @@
 class Support:
-    support = None
+    name = None
     dof = None
-    #Restraine types are:
+    #Support types are:
     #Fixed support(DOF = 0)
     #Pinned support(DOF = 1 Rotation)
     #Roller support(DOF = 2, horizontal and rotation)
     #No support(DOF = 3)
 
     def __init__(self, support):
-        self.support = support
+        self.name = support
         if support == "Fixed":
             self.dof = 0
         elif support == "Pinned":
             self.dof = 1
         elif support == "Roller":
-            self.dof = 2
+            self.dof = 1
         else:
-            self.dof = 3
+            self.dof = 2
